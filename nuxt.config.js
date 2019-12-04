e2482a9b48f9d4f45b95b19f1b157d5bb7256b9e
Callum Macdonald <git@callum-macdonald.com>
2019-12-04 15:57:09 +0100

    Prettier formatting again.

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,63 +1,65 @@
-const colors = require('vuetify/es5/util/colors').default
+const colors = require("vuetify/es5/util/colors").default;
 
 module.exports = {
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
-    title: 'kiezburn-purchasing',
+    title: "kiezburn-purchasing",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Receipt Processing and Purchasing for KiezBurn' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: "Receipt Processing and Purchasing for KiezBurn"
+      }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' }
+      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      {
+        rel: "stylesheet",
+        href:
+          "https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons"
+      }
     ]
   },
-  mode: 'spa',
+  mode: "spa",
   /*
-  ** Customize the progress bar color
-  */
-  loading: { color: '#3B8070' },
+   ** Customize the progress bar color
+   */
+  loading: { color: "#3B8070" },
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
     /*
-    ** Run ESLint on save
-    */
-    extend (config, { isDev, isClient }) {
+     ** Run ESLint on save
+     */
+    extend(config, { isDev, isClient }) {
       if (isDev && isClient) {
         config.module.rules.push({
-          enforce: 'pre',
+          enforce: "pre",
           test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
+          loader: "eslint-loader",
           exclude: /(node_modules)/
-        })
+        });
       }
     },
     extractCSS: true,
     analyze: {
-      analyzerMode: 'static'
+      analyzerMode: "static"
     },
-    vendor: [ 'firebase', 'vuetify' ]
+    vendor: ["firebase", "vuetify"]
   },
-  plugins: [
-    {src: '~/plugins/vuetify.js'},
-    '~/plugins/fireauth.js'
-  ],
-  modules: [
-    '@nuxtjs/pwa',
-    'nuxt-validate'
-  ],
-    /*
-  ** vuetify module configuration
-  ** https://github.com/nuxt-community/vuetify-module
-  */
- vuetify: {
-    customVariables: ['~/assets/variables.scss'],
+  plugins: [{ src: "~/plugins/vuetify.js" }, "~/plugins/fireauth.js"],
+  modules: ["@nuxtjs/pwa", "nuxt-validate"],
+  /*
+   ** vuetify module configuration
+   ** https://github.com/nuxt-community/vuetify-module
+   */
+  vuetify: {
+    customVariables: ["~/assets/variables.scss"],
     theme: {
       dark: true,
       themes: {
@@ -74,10 +76,10 @@ module.exports = {
     }
   },
   router: {
-    middleware: 'router-auth'
+    middleware: "router-auth"
   },
   css: [
-    { src: '~/assets/css/main.css', lang: 'css'},
-    { src: '~/assets/variables.scss', lang: 'scss'}
+    { src: "~/assets/css/main.css", lang: "css" },
+    { src: "~/assets/variables.scss", lang: "scss" }
   ]
-}
+};
